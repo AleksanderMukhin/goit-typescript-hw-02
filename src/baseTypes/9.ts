43eff693ca8c20ecfa825425f20329b43dbd3c1d
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-11-01 13:49:39 +0100

    fix

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,12 +2,16 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 type Page = {
-  title: string,
-  likes: number,
-  accounts: string[],
-  status: string,
-  details?: object,
-}
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: 'open' | 'close';
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+
 
 const page1: Page = {
   title: 'The awesome page',
